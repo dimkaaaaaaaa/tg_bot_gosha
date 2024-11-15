ee4c66092ad200ce490f8e36de4fbf7b62e4f43a
Dmitriy Sosnin <119790441+dimkaaaaaaaa@users.noreply.github.com>
2024-11-15 23:20:10 +0300

    added possibility to change the city

--- tgBot.py
+++ tgBot.py
@@ -8,6 +8,7 @@ import currentTime
 import currentWeather
 
 TOKEN = "7986596049:AAFtX6g_Q4iu9GBtG31giIONkUPd9oHmcYI"
+user_cities = {} # Словарь для хранения городов пользователей
 
 #Логирование
 logging.basicConfig(
@@ -17,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 # Стартовое сообщение
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    reply_keybord = [["Текущее время", "Погода в моем городе"]]
+    reply_keybord = [["Текущее время", "Погода в моем городе", "Изменить город"]]
     await update.message.reply_text(
         "Привет! Выберите действие:",
         reply_markup=ReplyKeyboardMarkup(reply_keybord, resize_keyboard=True, one_time_keyboard=True)
@@ -25,16 +26,28 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 # Обработка кнопок
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global user_cities
+    user_id = update.message.from_user.id
     text = update.message.text
+
+
     if text == "Текущее время" or text == "Время" or text == "время":
-        city = "Smolensk"
+        city = user_cities.get(user_id, "Moscow")
         curent_time = currentTime.get_current_time(city)
         now = datetime.now().strftime("%H:%M:%S")
         await update.message.reply_text(f"Текущее время в {city}: {curent_time}")
     elif text == "Погода в моем городе" or text == "Погода" or text == "погода":
-        city = "Smolensk"
+        city = user_cities.get(user_id, "Moscow")
         weather = currentWeather.get_weather(city)
         await update.message.reply_text(weather)
+    elif text == "Изменить город":
+        await update.message.reply_text("Напишите название нового города на английском языке.")
+        context.user_data["awaiting_city"] = True
+    elif context.user_data.get("awaiting_city"):
+        new_city = text
+        user_cities[user_id] = new_city
+        context.user_data["awaiting_city"] = False
+        await update.message.reply_text(f"Ваш город изменен на: {new_city}.")
     else:
         await update.message.reply_text("Выберите действие из предложенных.")
         
