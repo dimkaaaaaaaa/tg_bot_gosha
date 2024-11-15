7b4c19eac55defc0f0b2c82081e9cfa8e4809b77
Dmitriy Sosnin <119790441+dimkaaaaaaaa@users.noreply.github.com>
2024-11-15 11:05:15 +0300

    New code: Add current time, fix Time and Bug

---
+++ currentTime.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import requests
+import pytz
+
+def get_coordinates(city):
+    api_key = "bd5e378503939ddaee76f12ad7a97608"
+    url =  f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru"
+    response = requests.get(url).json()
+    if response.get("coord"):
+        return response["coord"]["lat"], response["coord"]["lon"]
+    else:
+        return None, None
+
+def get_timezone(lat, lon):
+    api_key = "6HHF7D61JGL5"
+    url = f"http://api.timezonedb.com/v2.1/get-time-zone?key={api_key}&format=json&by=position&lat={lat}&lng={lon}"
+    response = requests.get(url).json() 
+    if response.get("status") == "OK":
+        return response["zoneName"]
+    else:
+        return None
+
+def get_current_time(city):
+    lat, lon = get_coordinates(city)
+    if lat is not None and lon is not None:
+        timezone = get_timezone(lat, lon)
+        if timezone:
+            city_time = datetime.now(pytz.timezone(timezone))
+            return city_time.strftime("%H:%M:%S")
+        else:
+            return "Не удалось определить временную зону."
--- tgBot.py
+++ tgBot.py
@@ -5,21 +5,20 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from datetime import datetime
 import requests
+import currentTime
 
-
+app = Flask(__name__)
 
 TOKEN = "7986596049:AAFtX6g_Q4iu9GBtG31giIONkUPd9oHmcYI"
 
 #Логирование
 logging.basicConfig(
-    format='%(actime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
 # Функ для получения погоды
 def get_weather(city): 
-    
-    
     api_key = "bd5e378503939ddaee76f12ad7a97608"
     url =  f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru"
     response = requests.get(url).json()
@@ -52,10 +51,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 # Обработка кнопок
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     text = update.message.text
-    if text == "Текущее время":
+    if text == "Текущее время" or text == "Время" or text == "время":
+        city = "Smolensk"
+        curent_time = currentTime.get_current_time(city)
         now = datetime.now().strftime("%H:%M:%S")
-        await update.message.reply_text(f"Текущее время: {now}")
-    elif text == "Погода в моем городе":
+        await update.message.reply_text(f"Текущее время в {city}: {curent_time}")
+    elif text == "Погода в моем городе" or text == "Погода" or text == "погода":
         city = "Smolensk"
         weather = get_weather(city)
         await update.message.reply_text(weather)
@@ -71,5 +72,11 @@ def main():
 
     application.run_polling()
 
+
+@app.route('/')
+def index():
+    return "Bot is running!"
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    main() 
