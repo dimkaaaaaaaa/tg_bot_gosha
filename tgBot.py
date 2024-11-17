cd37250fa4f8be672a364888329c532d50c0e5cc
Dmitriy Sosnin <119790441+dimkaaaaaaaa@users.noreply.github.com>
2024-11-17 23:22:03 +0300

    remind2.3.2

--- tgBot.py
+++ tgBot.py
@@ -134,8 +134,4 @@ async def main():
     application.run_polling()
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except RuntimeError:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+    asyncio.run(main())
